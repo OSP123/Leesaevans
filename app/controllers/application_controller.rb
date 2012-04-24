7efffeda3d318b394153daf057784ae9dd6507b7
Omar Patel <omar@hybridgroup.com>
2012-04-23 22:32:24 -0700

    fixing redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_filter :ensure_domain
+  #before_filter :ensure_domain
 
-  APP_DOMAIN = 'www.leesaevans.com'
+  #APP_DOMAIN = 'www.leesaevans.com'
 
-  def ensure_domain
-    if Rails.env.production? && request.env['HTTP_HOST'] != 'leesaevansstaging.herokuapp.com'
-      if request.env['HTTP_HOST'] != APP_DOMAIN
-        # HTTP 301 is a "permanent" redirect
-        redirect_to "http://#{APP_DOMAIN}", :status => 301
-      end
-    end
-  end
+  #def ensure_domain
+    #if Rails.env.production? && request.env['HTTP_HOST'] != 'leesaevansstaging.herokuapp.com'
+      #if request.env['HTTP_HOST'] != APP_DOMAIN
+         #HTTP 301 is a "permanent" redirect
+        #redirect_to "http://#{APP_DOMAIN}", :status => 301
+      #end
+    #end
+  #end
 end
